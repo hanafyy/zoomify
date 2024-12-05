30cd117789180c31e0591be8c273961b04a209a2
hanafyy <omarhanafy0@gmail.com>
2024-12-05 17:23:54 +0200

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         layout: {
           socialButtonsVariant: 'iconButton',
